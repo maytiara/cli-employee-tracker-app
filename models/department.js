320722bd4112e860d3c4d904d140e888b851fb52
May Carandang <maytiara@gmail.com>
2022-08-10 17:21:52 +0800

    Added (2) functions to be imported in main js file

---
+++ models/department.js
@@ -0,0 +1,14 @@
+// these functions will be imported to the app.js (or main/server js)
+function addDepart() {
+
+}
+
+function getDepart() {
+
+}
+
+module.exports = {
+
+  addDepart(),
+  getDepart(),
+}
