9ba4470674e2800389216ae9da6aaddb9524ae6e
May Carandang <maytiara@gmail.com>
2022-08-10 19:24:30 +0800

    Added switch case statement together with a series of cause & break clauses.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 // CREATE > table using mySQL -->done using ./db/schema.sql
 
 const inquirer = require('inquirer');
+const { getDepart } = require('./models/department');
 
 inquirer.prompt ([
   {
@@ -18,9 +19,23 @@ inquirer.prompt ([
       "Add Roles",
       "Add An Employee",
       "Update Employee Role",
+      "exit",
     ]
   }
-])
+]).then(async (ans) => {
+
+  switch(ans.choices){
+
+    case "View All Departments":
+      const departments = await getDepart();
+      console.table(departments);
+      break;
+
+    // Once the user, select the exit, the node environment will stop the process.
+    case "exit":
+      process.exit(0);
+  }
+})
 
 // User inputs: 
 // VIEW ALL > departments, roles, employees, 
