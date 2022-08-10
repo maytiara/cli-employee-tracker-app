bc398762b074a2896a2e898e1ffe4ceacfc494d1
May Carandang <maytiara@gmail.com>
2022-08-10 17:16:27 +0800

    Added initial pseudo codes

---
+++ app.js
@@ -0,0 +1,10 @@
+// Start of building the application
+// npm init > install required npm packages
+// .gitignore > necessary files
+// CREATE > table using mySQL -->done using ./db/schema.sql
+
+// User inputs: 
+// VIEW ALL > departments, roles, employees, 
+// ADD > department, a role, an employee, 
+// UPDATE > an employee role
+
