1c222fe8f0e6af057fc77597b0511041c0bda272
May Carandang <maytiara@gmail.com>
2022-08-10 22:53:44 +0800

    Added function to connect mysql database, and used async & await method

--- models/department.js
+++ models/department.js
@@ -1,10 +1,14 @@
+const { createConnect } = require("../db/connection"); // added automatically by VS C
+
 // these functions will be imported to the app.js (or main/server js)
 function addDepart() {
 
 }
 
-function getDepart() {
-
+async function getDepart() {
+  
+  const db = await createConnect();
+  db.query(sql)
 }
 
 module.exports = {
