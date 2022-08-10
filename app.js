ff0904855a6eaf2bb06bc8468b6111427d5eb863
May Carandang <maytiara@gmail.com>
2022-08-10 17:26:14 +0800

    Added require function for first inquirer prompt [ ]

--- app.js
+++ app.js
@@ -3,6 +3,14 @@
 // .gitignore > necessary files
 // CREATE > table using mySQL -->done using ./db/schema.sql
 
+const inquirer = require('inquirer');
+
+inquirer.prompt ([
+  {
+    
+  }
+])
+
 // User inputs: 
 // VIEW ALL > departments, roles, employees, 
 // ADD > department, a role, an employee, 
