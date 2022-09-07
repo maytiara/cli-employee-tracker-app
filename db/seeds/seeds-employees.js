7a02dc982a0262db3d55b89f05dafd3217d030fe
May Carandang <maytiara@gmail.com>
2022-09-07 20:08:18 +0800

    Added pseudo code, and for optional usage only

--- db/seeds/seeds-employees.js
+++ db/seeds/seeds-employees.js
@@ -1,10 +1,20 @@
-const {faker} = require('@faker-js/faker');
+const { faker } = require('@faker-js/faker'); //npm package
+const { addEmployee } = require('../../models/employee');
 
-
-
-async function seedEmployees(num = 17){
+// For loop function generate fake data name
+async function seedEmployees(num = 16){
   for (let index = 0; index < num; index++) {
 
-    await getEmployees(faker.name.findName)
+    await addEmployee({
+      first_name: faker.name.firstName(),
+      last_name:faker.name.lastName(),
+    });
   }
+  return addEmployee;
 }
+
+
+// this exports to 
+module.exports = { seedEmployees };
+
+// Optional ONLY! Hard coded the data in mySQL 
