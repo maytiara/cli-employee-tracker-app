56f9e830036fd16c6d0669d93e857a2156885d7f
May Carandang <maytiara@gmail.com>
2022-09-15 00:18:10 +0800

    comment out the unused lines

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,12 +1,12 @@
 // this file to run npm run seed
 
-const { seedEmployees } = require("./seeds-employees");
-
-async function main(){
-
-  await seedEmployees();
-}
-
-main();
+// const { seedEmployees } = require("./seeds-employees");
+// 
+// async function main(){
+// 
+//   await seedEmployees();
+// }
+// 
+// main();
 
 // Optional ONLY! Hard coded the data in mySQL 
--- db/seeds/seeds-employees.js
+++ db/seeds/seeds-employees.js
@@ -1,20 +1,20 @@
-const { faker } = require('@faker-js/faker'); //npm package
-const { addEmployee } = require('../../models/employee');
-
-// For loop function generate fake data name
-async function seedEmployees(num = 16){
-  for (let index = 0; index < num; index++) {
-
-    await addEmployee({
-      first_name: faker.name.firstName(),
-      last_name:faker.name.lastName(),
-    });
-  }
-  return addEmployee;
-}
-
-
-// this exports to 
-module.exports = { seedEmployees };
+// const { faker } = require('@faker-js/faker'); //npm package
+// const { addEmployee } = require('../../models/employee');
+// 
+// // For loop function generate fake data name
+// async function seedEmployees(num = 16){
+//   for (let index = 0; index < num; index++) {
+// 
+//     await addEmployee({
+//       first_name: faker.name.firstName(),
+//       last_name:faker.name.lastName(),
+//     });
+//   }
+//   return addEmployee;
+// }
+// 
+// 
+// // this exports to 
+// module.exports = { seedEmployees };
 
 // Optional ONLY! Hard coded the data in mySQL 
