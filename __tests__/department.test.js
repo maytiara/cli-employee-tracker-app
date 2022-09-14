bfb600b63909f24cfa93486794a41088f8183c51
May Carandang <maytiara@gmail.com>
2022-09-14 20:58:53 +0800

    using mock testing, to test the connection & passed the addDepart function

--- __tests__/department.test.js
+++ __tests__/department.test.js
@@ -1,8 +1,23 @@
 const { addDepart } = require ("../models/department");
 
+const { createConnect } = require("../connection/connection");
+
+jest.mock('../connection/connection') //for the dummy connection
+
 test ("addDepart() should return an update department", async () => {
 
-  await addDepart("Department Name"); //this "Department Name" saved in database when running the test 
-  expect("").toEqual(""); // expected to be undefined.
-  return ""; 
+  // arrange
+  const test_data= 'test data' //variable for the 'string data'
+  let query = jest.fn(() => Promise.resolve([test_data]));
+
+  createConnect.mockImplementation(() => ({
+    query // 
+  }))
+
+  // act
+  const result = await addDepart(test_data); //assigned variable for the addDepart passing a 'test_data'
+  
+  // assert
+  expect(query).toHaveBeenCalled()
+  expect(result).toBe(test_data)
 });
